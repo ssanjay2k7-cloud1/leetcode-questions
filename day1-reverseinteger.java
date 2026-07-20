9b988c3da624aff15dab5278d4f9acf8c5b0f5f5
ssanjay2k7-cloud1 <ssanjay2k7@gmail.com>
2026-07-20 08:57:13 +0530

    Create day1-reverseinteger.java

---
+++ day1-reverseinteger.java
@@ -0,0 +1,18 @@
+class Solution { 
+    public int reverse(int x) { 
+        long reverse = 0; 
+        
+        while (x != 0) { 
+            int last = x % 10; 
+            reverse = (reverse * 10) + last; 
+            x = x / 10;                      
+        } 
+        
+        
+        if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) { 
+            return 0; 
+        } 
+        
+        return (int) reverse; 
+    } 
+}
